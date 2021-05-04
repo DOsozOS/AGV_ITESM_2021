f217d9fa30bc2f50739b6706d4d5cb39e2e1803e
DOsozOS <50999954+DOsozOS@users.noreply.github.com>
2021-05-04 18:20:26 -0500

    Add files via upload

---
+++ ORB_SLAM2/tum1.sh
@@ -0,0 +1,1 @@
+./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.bin Examples/RGB-D/TUM1.yaml /media/king/Lenovo/硕士/image/rgbd_dataset_freiburg1_xyz /media/king/Lenovo/硕士/image/rgbd_dataset_freiburg1_xyz/associate.txt
